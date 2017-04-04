d6a74a0d7888aa64a552aa416052b467b8b4cc2f
Nalinda Karunaratna <NalindaKarunaratna@gmail.com>
2017-04-05 07:18:49 +1000

    output origin access identity

--- cloudfront/outputs.tf
+++ cloudfront/outputs.tf
@@ -5,3 +5,7 @@ output "distribution_name" {
 output "distribution_zone_id" {
   value = "${aws_cloudfront_distribution.cloudfront.hosted_zone_id}"
 }
+
+output "origin_access_identity_id" {
+  value = "${aws_cloudfront_origin_access_identity.origin_access_identity.id}"
+}
